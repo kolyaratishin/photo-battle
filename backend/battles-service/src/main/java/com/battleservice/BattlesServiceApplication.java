4b37182ae478e04f7ff2dfa3be63d584b9d75e61
kolyaratishin <mykola.ratyshyn@eleks.com>
2024-02-19 15:35:48 +0200

    init battle service

---
+++ backend/battles-service/src/main/java/com/battleservice/BattlesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.battleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlesServiceApplication.class, args);
+	}
+
+}
---
+++ backend/battles-service/src/test/java/com/battleservice/BattlesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
